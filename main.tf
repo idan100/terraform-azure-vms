e34f0f12d7b9e1201d1588c8986e824880cee47e
idan100 <31824300+idan100@users.noreply.github.com>
2023-09-12 12:05:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "main" {
   name                = "${var.vm_name}-nic-${count.index}"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  tags                = "${var.tags}"
+  tags                = {application="gaia"}
   count               = "1"
 
   ip_configuration {
