0e7c3ce87500470d1592c50e9c1275d469cca20d
idan100 <31824300+idan100@users.noreply.github.com>
2023-09-12 12:31:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vnet_name}"
   location            = "${var.location}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.65.0.0/16"]
   resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
